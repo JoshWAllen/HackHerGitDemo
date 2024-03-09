f63b1c90efd47a2ccdf539d5ece9192cb63a628a
Sophie Robbins <s.robbins6@yahoo.com>
2024-03-09 15:57:53 -0500

    Co-authored-by: Anna <annawu04@users.noreply.github.com> Co-authored-by: Josh Allen <JoshWAllen@users.noreply.github.com> Co-authored-by: emmaheff123 <emmaheff123@users.noreply.github.com>

---
+++ trapp/src/routes/sleep/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+
+</script>
+
+<h1>Sleep</h1>
+<h4>See your mental score insights</h4>
+
+<div class="card">
+    <h2>Rank your mood today:</h2> 
+</div>
+
+<div class="custom-select" style="width:100px;">
+    <select id="ranking">
+        <option value="0">Select 1-10 ranking:</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+    </select>
+</div>
+
+<style>
+    h1 {
+        color: blue;
+        font-size: 100px;
+        padding: 1rem;
+        margin: 0 0;
+    }
+
+    h4 {
+        color: lightblue;
+        font-size: 20px;
+        padding: 1rem;
+        margin: 0 0;
+    }
+
+    h2 {
+        color: lightblue;
+        font-size: 20px;
+        padding: 1rem;
+        margin: 0 0;
+    }
+</style>
